99c1581c80781e191e0c670beb19b2b87ecb7c1a
Leonid Babikov <komsldt@gmail.com>
2015-02-17 20:46:47 +0300

    added cron job for news

---
+++ cron/getLatestNews.php
@@ -0,0 +1,27 @@
+<?php
+
+if (class_exists("Memcache")) {
+    $mc = new Memcache();
+    if ($mc->connect('127.0.0.1', 11211) !== false){
+        if($mc->get('curl_csgo')===false){
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'http://store.steampowered.com/api/appdetails/?appids=730');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3'
+            ));
+            $answer = curl_exec($ch);
+            $json_news = json_decode($answer);
+            if($json_news!==false){
+                echo "setted\n";
+                $mc->set('curl_csgo',$answer,MEMCACHE_COMPRESSED,3600);
+            }
+        } else {
+            echo "in memcache\n";
+        }
+
+    }
+}
+
