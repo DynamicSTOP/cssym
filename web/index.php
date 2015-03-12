c8d371986d5e8ab9b9b843f9f89f9f28361b953e
Leonid Babikov <komsldt@gmail.com>
2015-03-12 21:25:45 +0300

    fixed redirect. for some reason it doesn't work properly inside try block

--- web/index.php
+++ web/index.php
@@ -18,10 +18,11 @@ $app->get('/login', function () use ($app) {
         try {
             $openid = new LightOpenID("http://{$_SERVER['HTTP_HOST']}/login");
             $openid->identity = 'http://steamcommunity.com/openid/?l=russian';
-            $app->redirect($openid->authUrl());
-        } catch(Exception $e){
-            $app->redirect("/technicalIssues");
+            $url=$openid->authUrl();
+        } catch(\Exception $e){
+            $url="/technicalIssues";
         }
+        $app->redirect($url);
     } else if ($openid->mode != 'cancel') {
         if ($openid->validate()) {
             $id = $openid->identity;
@@ -44,8 +45,6 @@ $app->get('/logout', function () use ($app) {
 $app->group('/user', $redirectIfNotLoggedIn, function() use($app,$entityManager, $data){
     $app->get('/checkForAdmin', function () use ($app, $entityManager, $data) {
         //TODO this method looks like a mess. u probably want to have a controller finally
-        if (!isset($_SESSION['user']['u']))
-            $app->redirect("/");
         $userRepository = $entityManager->getRepository("\\WebApp\\Entity\\User");
         $user = $userRepository->findOneBy(array("steamId" => $_SESSION['steamId']));
 
